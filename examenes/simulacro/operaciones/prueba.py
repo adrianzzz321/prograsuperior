fc53212a95f14868aa8a7d22458d6e66d45f9d90
adrianzzz321 <adralanoca20@gmail.com>
2025-05-21 23:44:21 -0400

    simulacro bien hecho

--- examenes/simulacro/operaciones/prueba.py
+++
@@ -1,8 +0,0 @@
-numero=int(input("ds"))
-cont =0
-for i in range (1, numero):
-        guardador=numero
-        while guardador>9:
-            guardador-=10
-        if i%3==0 or guardador==3:
-            cont+=1
